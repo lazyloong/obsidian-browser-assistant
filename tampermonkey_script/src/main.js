a788fcc770d10ed475c7da401001044937276002
lazyloong <2950449004@qq.com>
2023-02-06 16:22:00 +0800

    1.1 update
    可以使用核心插件的搜索功能

--- tampermonkey_script/src/main.js
+++ tampermonkey_script/src/main.js
@@ -16,15 +16,15 @@ const regex = new RegExp(
     "https:\\/\\/cn\\.bing\\.com\\/search\\?q=([^&]*)(&.*)?"
 );
 let query = regex.exec(url)[1];
-console.log(GM_getValue("ba_port"));
 let post_url = "http://127.0.0.1:" + (GM_getValue("ba_port") ?? "27125");
 
 let target_block = document
     .querySelector("div#b_content")
     .querySelector('[aria-label="更多结果"]')
     .querySelector("ol");
-let n = document.createElement("div");
-target_block.insertBefore(n, target_block.firstChild);
+let newblock = document.createElement("div");
+target_block.insertBefore(newblock, target_block.firstChild);
+const app = ReactDOM.render(<App />, newblock);
 
 let httpRequest = new XMLHttpRequest();
 httpRequest.open("POST", post_url, true);
@@ -33,6 +33,6 @@ httpRequest.onreadystatechange = function () {
     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
         let json = httpRequest.responseText,
             data = JSON.parse(json);
-        ReactDOM.render(<App data={data} />, n);
+        app.switch(data);
     }
 };
