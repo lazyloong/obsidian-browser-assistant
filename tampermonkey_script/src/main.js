85a8b0e12dc0ecee74471bc45251aeaa148bd6a9
lazyloong <2950449004@qq.com>
2024-03-05 22:37:13 +0800

    feat: 由于omnisearch直接支持服务器，因此删除插件

--- tampermonkey_script/src/main.js
+++ tampermonkey_script/src/main.js
@@ -1,38 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
+import EdgeLoader from "./edge";
 
 GM_registerMenuCommand("切换端口", function () {
     let port = GM_getValue("ba_port");
     if (!port) {
-        port = 27125;
+        port = 51361;
     }
     port = window.prompt("设置可用端口", port);
     GM_setValue("ba_port", port);
 });
 
 const url = window.location.href;
-const regex = new RegExp(
-    "https:\\/\\/cn\\.bing\\.com\\/search\\?q=([^&]*)(&.*)?"
-);
-let query = regex.exec(url)[1];
-let post_url = "http://127.0.0.1:" + (GM_getValue("ba_port") ?? "27125");
-
-let target_block = document
-    .querySelector("div#b_content")
-    .querySelector('[aria-label="更多结果"]')
-    .querySelector("ol");
-let newblock = document.createElement("div");
-target_block.insertBefore(newblock, target_block.firstChild);
-const app = ReactDOM.render(<App />, newblock);
-
-let httpRequest = new XMLHttpRequest();
-httpRequest.open("POST", post_url, true);
-httpRequest.send(JSON.stringify({ query: query }));
-httpRequest.onreadystatechange = function () {
-    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-        let json = httpRequest.responseText,
-            data = JSON.parse(json);
-        app.switch(data);
-    }
+const loaders = {
+    edge: new EdgeLoader(),
 };
+let loader;
+for (let key in loaders) {
+    if (loaders[key].test(url)) {
+        loader = loaders[key];
+        break;
+    }
+}
+let port = GM_getValue("ba_port") ?? "51361";
+loader.load(url, port);
